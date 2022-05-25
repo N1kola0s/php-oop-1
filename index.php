76dd46a39a19a61b5a4c25876d04c54466222aa5
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-25 20:02:33 +0200

    add array push method for films array

--- index.php
+++ index.php
@@ -36,15 +36,26 @@ class Movie {
 
 }
 
+$films = [];
+
 $batman = new Movie('https://image.tmdb.org/t/p/w342/8o1R5QU797hcLuvU4M9F3rOBGEY.jpg', 'TheBatman', 'en', 7.8, 'Quando un killer prende di mira l’élite di Gotham con una serie di malvagi stratagemmi, una scia di indizi criptici spinge il più grande detective del mondo a indagare nei bassifondi, incontrando personaggi come Selina Kyle alias Catwoman, Oswald Cobblepot alias il Pinguino, Carmine Falcone e Edward Nashton alias l’Enigmista. Mentre le prove iniziano a condurlo più vicino alla soluzione e la portata dei piani del malfattore diventa chiara, Batman deve stringere nuove alleanze, smascherare il colpevole e rendere giustizia all’abuso di potere e alla corruzione che da tempo affliggono Gotham City.');
 $batman -> setVoteRounding(7.8);
-var_dump($batman);
+/* var_dump($batman); */
 
 $spiderman = new Movie ('https://image.tmdb.org/t/p/w342/gh4cZbhZxyTbgxQPxD0dOudNPTn.jpg', 'Spiderman', 'en', 7.2, 'Peter Parker è il tipico adolescente sfigato, segretamente innamorato fin da bambino della sua vicina di casa Mary Jane, rimasto orfano dei genitori e che vive con gli zii. Durante una gita scolastica a un laboratorio scientifico Peter viene morso da un ragno geneticamente modificato. Ben presto si renderà conto di non aver più bisogno degli occhiali da vista e di essere in possesso di poteri molto particolari.');
 $spiderman -> setVoteRounding(7.2);
 
+$superman = new Movie ('https://image.tmdb.org/t/p/w342/ucdkkitvvTKKmfzLuiKFCirn5j2.jpg', 'Superman', 'en', 7.1, 'Sul pianeta Kripton é il 1948 quando il tribunale sta giudicando tre traditori. Il presidente del tribunale manda suo figlio Kal-El in salvo sulla terra, dove a causa della diversa atmosfera assume poteri straordinari. Viene adottato da una copia diventando così Clark Kent.');
+$superman -> setVoteRounding(7.1);
 
-$films = [
+
+
+
+array_push($films, $batman, $spiderman, $superman);
+
+
+
+/* $films = [
     $batman,
     $spiderman,
     new Movie('https://image.tmdb.org/t/p/w342/8o1R5QU797hcLuvU4M9F3rOBGEY.jpg', 'TheBatman', 'en', 7.8, 'Quando un killer prende di mira l’élite di Gotham con una serie di malvagi stratagemmi, una scia di indizi criptici spinge il più grande detective del mondo a indagare nei bassifondi, incontrando personaggi come Selina Kyle alias Catwoman, Oswald Cobblepot alias il Pinguino, Carmine Falcone e Edward Nashton alias l’Enigmista. Mentre le prove iniziano a condurlo più vicino alla soluzione e la portata dei piani del malfattore diventa chiara, Batman deve stringere nuove alleanze, smascherare il colpevole e rendere giustizia all’abuso di potere e alla corruzione che da tempo affliggono Gotham City.'),
@@ -53,7 +64,7 @@ $films = [
 
     new Movie ('https://image.tmdb.org/t/p/w342/ucdkkitvvTKKmfzLuiKFCirn5j2.jpg', 'Superman', 'en', 7.1, 'Sul pianeta Kripton é il 1948 quando il tribunale sta giudicando tre traditori. Il presidente del tribunale manda suo figlio Kal-El in salvo sulla terra, dove a causa della diversa atmosfera assume poteri straordinari. Viene adottato da una copia diventando così Clark Kent.'),
 
-];
+]; */
 
 
 ?>
